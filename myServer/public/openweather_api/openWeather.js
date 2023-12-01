fe7b48dd2162fc2781639cea52f700fbfb1586d1
jAlbright2002 <146133052+jAlbright2002@users.noreply.github.com>
2023-12-01 14:06:50 +0000

    Co-authored-by: MarkVeerasingam <MarkVeerasingam@users.noreply.github.com>

--- myServer/public/openweather_api/openWeather.js
+++ myServer/public/openweather_api/openWeather.js
@@ -1,7 +1,8 @@
 //https://alwazkazi3.medium.com/creating-a-weather-app-using-api-javascript-4d7bb26bbc92
 // https://api.openweathermap.org/data/2.5/weather?q=Galway,ie&units=metric&appid=8dba034299cc23d31370692738fac593
 
-const KEY = '8dba034299cc23d31370692738fac593';
+//const KEY = '8dba034299cc23d31370692738fac593';
+const KEY = '1234'; // this is to save api calls a day
 
 // ACCESSING ALL THE HTML COMPONENTS REQUIRED TO PERFORM ACTIONS ON.
 let button = document.querySelector('.button')
@@ -12,7 +13,7 @@ let desc = document.querySelector('.desc');
 
 
 // ADDING EVENT LISTENER TO SEARCH BUTTON  
-button.addEventListener('click', function(){
+function updateWeather(){
 
     // Fection data from open weather API
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=Galway,ie&units=metric&appid=${KEY}`)
@@ -20,12 +21,16 @@ button.addEventListener('click', function(){
     .then(
         displayData)
     .catch(err => alert('Wrong City name')); 
-})
+}
 
 // Function to diplay weather on html document
 const displayData=(weather)=>{
     temp.innerText=`${weather.main.temp}°C`
     desc.innerText=`${weather.weather[0].main}`
 }
+
+window.onload = updateWeather();
+
+setInterval(updateWeather, 60 * 60 * 1000);
 
     
