a573038cccf09bcd743166fc5a0cad95986a3e11
MarkVeerasingam <122292447+MarkVeerasingam@users.noreply.github.com>
2023-12-05 12:35:50 +0000

    added some comments

---
+++ myServer/resetServerScript.mjs
@@ -0,0 +1,8 @@
+/*
+    Brian suggests a scirpt that runs in the background that tests the connection of the website every 10 seconds
+    and if the website has a bad connection or times out it resets the server
+
+    look at linux reboot cmnd
+*/
+
+
